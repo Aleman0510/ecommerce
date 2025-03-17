a17e4f77d24573a1efc8c128da4701539f40600a
Aram Aleman <alemancardenasaram@gmail.com>
2025-03-16 20:48:39 -0600

    creacion con makemigrations

--- myshop/shop/models.py
+++ myshop/shop/models.py
@@ -16,17 +16,17 @@ class Category (models.Model):
     def __str__(self):
         return self.name
     
-class Product(models.Model):                                
-    category = models.ForeignKey(
-        Category,
-        related_name='products',
-        on_delete=models.CASCADE
-    )
+class Product(models.Model):
+    category = models.ForeignKey(Category,
+                                 related_name='products',
+                                 on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
     slug = models.SlugField(max_length=200)
-    image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
+    image = models.ImageField(upload_to='products/%Y/%m/%d',
+                              blank=True)
     description = models.TextField(blank=True)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    price = models.DecimalField(max_digits=10,
+                                decimal_places=2)
     available = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
